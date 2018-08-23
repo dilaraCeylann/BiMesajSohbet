323429285f316a3456d3719642a6fa83ddd8732c
Ömer Sina Ceylan <portakaorda44@gmail.com>
2018-08-23 11:04:47 +0300

    Yeni Deneme

--- app/src/main/java/com/elias/bimesajsohbet/MainActivity.java
+++ app/src/main/java/com/elias/bimesajsohbet/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
     private TabsPagerAdapter myTabsPagerAdapter;
-
+    //lalala
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
